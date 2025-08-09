9e0d5c506fa3e265b805342ada28996b802d314c
“Nani” <sathish.palle@omnihospitals.in>
2025-08-09 15:04:04 +0530

    all componenets added

--- omni-project-frontend/src/app/app.component.spec.ts
+++ omni-project-frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'omni-project-frontend'`, () => {
+  it(`should have as title 'Omni_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('omni-project-frontend');
+    expect(app.title).toEqual('Omni_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('omni-project-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Omni_project app is running!');
   });
 });
